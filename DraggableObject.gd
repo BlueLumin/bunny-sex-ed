dcb796bedb23ef7a29b1f1570b9e0eeab8f13e15
AustenvdByl <austenvdb@gmail.com>
2025-03-22 14:32:34 +0200

    basic draggable functionality

---
+++ DraggableObject.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+var dragging = false
+var of = Vector2(0,0)
+
+func _process(delta):
+	if dragging:
+		position = get_global_mouse_position()
+	
+
+func _on_button_button_down() -> void:
+	dragging = true
+	
+
+
+func _on_button_button_up() -> void:
+	dragging = false
