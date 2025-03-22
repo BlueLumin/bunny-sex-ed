01d2af0a04af33785b662640bd91753f9e481882
BlueLumin <78796040+BlueLumin@users.noreply.github.com>
2025-03-22 17:36:56 +0200

    Bunny Animations

--- Austen's Hellhole/BunnyEffectsController.gd
+++ Austen's Hellhole/BunnyEffectsController.gd
@@ -1,11 +1,13 @@
-extends CharacterBody2D
+extends Node2D
 
 @export var _bunnyAnimations: AnimatedSprite2D
-@export var _spawnAudio: Array[AudioStreamPlayer2D]
-@export var _clickedAudio: Array[AudioStreamPlayer2D]
-@export var _droppedAudio: Array[AudioStreamPlayer2D]
-@export var _interruptAudio: Array[AudioStreamPlayer2D]
+@export var bunny_click_audio_player: AudioStreamPlayer
+@export var bunny_drop_audio_player: AudioStreamPlayer
+@export var bunny_climax_audio_player: AudioStreamPlayer
+@export var bunny_inturrupt_audio_player: AudioStreamPlayer
+@export var bunny_spawn_audio_player: AudioStreamPlayer
 @export var _heatColor: Color
+
 
 func playIdle():
 	_bunnyAnimations.play("Idle")
@@ -14,32 +16,31 @@ func playIdle():
 func playWalking():
 	_bunnyAnimations.play("Walking")
 
+
 func playFucked():
 	_bunnyAnimations.play("Fucked")
 	
+	
 func playSpawn():
 	_bunnyAnimations.play("Spawn")
-	playAudioAtRandomPitch(_spawnAudio[randi_range(0, _spawnAudio.size() - 1)], 0.9, 1.1)
+	bunny_spawn_audio_player.play()
 
 
 func playLearning():
 	_bunnyAnimations.play("Learning")
 
+
 func playClicked():
 	_bunnyAnimations.play("Clicked")
-	playAudioAtRandomPitch(_clickedAudio[randi_range(0, _clickedAudio.size() - 1)], 0.9, 1.1)
+	bunny_click_audio_player.play()
 
 	
 func playDropped():
 	_bunnyAnimations.play("Idle")
-	playAudioAtRandomPitch(_droppedAudio[randi_range(0, _droppedAudio.size() - 1)], 0.9, 1.1)
+	bunny_drop_audio_player.play()
 
 
 func playInterrupt():
 	_bunnyAnimations.play("Interrupt")
-	playAudioAtRandomPitch(_interruptAudio[randi_range(0, _interruptAudio.size() - 1)], 0.9, 1.1)
+	bunny_inturrupt_audio_player.play()
 	
-
-func playAudioAtRandomPitch(audio: AudioStreamPlayer2D, min: float, max : float):
-	audio.pitch_scale = randf_range(min, max)
-	audio.play()
